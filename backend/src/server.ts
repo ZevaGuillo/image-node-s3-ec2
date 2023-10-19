43936425920a94dedd99be305c8bb30d37d18b62
Guillermo Zevallos <guiller.zeva16@gmail.com>
2023-10-18 19:56:35 -0500

    🔒 feat: Add CORS support in the backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express, { Router } from 'express';
 import path from 'path';
+import cors from 'cors'
 
 interface Options {
   port: number;
@@ -24,7 +25,8 @@ export class Server {
   async start() {
     //* Middlewares
     this.app.use( express.json() ); // raw
-    this.app.use( express.urlencoded({ extended: true }) ); // x-www-form-urlencoded
+    this.app.use( express.urlencoded({ extended: true }) );
+    this.app.use( cors() ) // x-www-form-urlencoded
 
     // //* Public Folder
     // this.app.use( express.static( this.publicPath ) );
